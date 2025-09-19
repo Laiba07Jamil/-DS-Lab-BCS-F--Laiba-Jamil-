b64e3be0d1971765bbb8d976f319666e278b970c
Laiba Jamil <laibajamil2007@gmail.com>
2025-09-19 12:09:59 +0500

    Update q2.cpp

--- Lab 5/q2.cpp
+++ Lab 5/q2.cpp
@@ -1,22 +1,46 @@
+/*Write a simple C++ program that demonstrates direct recursion. Create a function
+printNumbers(int n) that prints numbers from n down to 1 by calling itself
+directly.
+2. Next, demonstrate indirect recursion by creating two functions, functionA(int n)
+and functionB(int n). functionA should call functionB, and functionB
+should call functionA, forming a cycle that prints numbers in a specific pattern.
+Ensure both programs have a base condition to prevent infinite loops.*/
+
 #include<iostream>
 using namespace std;
 
-	int sumOfN(int x, int n)
-	
-	{
-		if(n==0)
-		{
-			return 1;
-		}
-		return  x*sumOfN(x,n-1);
-	}
-	
-	int main()
-	{
-		int x,n;
-		cout<<"Enter the number: ";
-		cin>>x;
-		cout<<"Enter the power: ";
-		cin>>n;
-		cout<<"power: "<<sumOfN(x,n);
-	}
+void print(int n){
+    if(n < 1 ){
+        return;
+    }
+    cout << n << " ";
+    return print(n - 1);
+}
+void functionB(int n);
+
+void functionA(int n){
+    if(n < 1){
+        return;
+    }
+    cout << n << " ";
+    return functionB(n - 1);
+}
+
+void functionB(int n){
+    if(n < 1){
+        return;
+    }
+    cout << n << " ";
+    return functionA(n - 1);
+}
+
+int main(){
+    int n;
+    cout <<"Enter the element to print: ";
+    cin >> n;
+    cout << "Type 1 recursion " << endl;
+    print(n);
+    cout << endl;
+    cout << "Type 2 recursion " << endl;
+    functionA(n);
+}
