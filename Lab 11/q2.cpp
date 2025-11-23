5b680751c5cc00cced3a8fcd01cb679b4558460b
Laiba Jamil <laibajamil2007@gmail.com>
2025-11-23 10:56:41 +0500

    Update q2.cpp

--- Lab 11/q2.cpp
+++ Lab 11/q2.cpp
@@ -1,44 +1,41 @@
 #include <iostream>
 using namespace std;
 
-const int TABLE_SIZE = 7;
+#define TABLE_SIZE 100
 
 class Node{
     public:
-    string key;
     string value;
+    string key;
     Node* next;
-    Node(string k , const string& v){
+    Node(string k , string v){
+        next = NULL;
         key = k;
         value = v;
     }
 };
 
 class HashTable{
-    Node* table[TABLE_SIZE];
-
+    Node* Table[TABLE_SIZE];
     public:
     HashTable(){
         for(int i = 0 ; i < TABLE_SIZE ; i++){
-            table[i] = nullptr;
+            Table[i] = nullptr;
         }
     }
 
     int hashfunction(string key){
-        int n = key.size();
         int sum = 0;
-        for(int i = 0 ; i < n ; i++){
-            char c = key[i];
-            sum += c ;
+        for(int i = 0 ; i < key.size() ; i++){
+            sum += key[i];
         }
         return sum % TABLE_SIZE;
     }
 
-    void insert(string key , string value){
+    void add_record(string key , string value){
         int index = hashfunction(key);
-
-        Node* temp = table[index];
-        while(temp != nullptr){
+        Node* temp = Table[index];
+        while(temp != NULL){
             if(temp->key == key){
                 temp->value = value;
                 return;
@@ -47,43 +44,87 @@ class HashTable{
         }
 
         Node* newnode = new Node(key , value);
-        newnode->next = table[index];
-        table[index] = newnode;
+        newnode->next = Table[index];
+        Table[index] = newnode;
+    }
+
+    void search(string key){
+        int index = hashfunction(key);
+        Node* temp = Table[index];
+        while(temp != NULL){
+            if(temp->key == key){
+                cout << "search for " << key << ": " << temp->value << endl;
+                return;
+            }
+            temp = temp->next;
+        }
+        cout << "Search word not found. " << endl;
+    }
+
+    void remove(string key){
+        int index = hashfunction(key);
+        Node* temp = Table[index];
+        Node* prev = nullptr;
+
+        if(temp == NULL){
+            cout << "key not found for deletion." << endl;
+            return ;
+        }
+
+        while(temp != NULL & temp->key != key){
+            prev = temp;
+            temp = temp->next;
+        }
+        if(temp->key == key){
+            Table[index] = temp->next;
+        }
+        else{
+            prev->next = temp->next;
+        }
+        cout << "key " << key << " deleted successfully." << endl;
+        delete temp;
+
     }
 
     void traverse(){
-        for(int i = 0 ; i < TABLE_SIZE ; i++){
-             Node* temp = table[i];
+        for(int i  = 0 ; i < TABLE_SIZE ; i++){
+            Node* temp = Table[i];
             if(temp == NULL){
-                    continue;
-                }
-            cout << "[" << i << "]: " ;
-           
+                continue;
+            }
+            cout << "[" << i  << "]" ;
             while(temp != NULL){
-                cout << "(" << temp->key << " , " << temp->value << ")" ;
+                cout << "(" << temp->key << "," << temp->value << ")";
                 Node* curr = temp->next;
-
                 while(curr != NULL){
-                    cout << "->" ;
-                     cout << "(" << curr->key << " , " << curr->value << ")" ;
-                     curr= curr->next;
-                }
+                    cout  << "->";
+                    cout << "(" << temp->key << "," << temp->value << ")" ;
+                    curr = curr->next;
+                } 
                 cout << endl;
                 temp = NULL;
             }
-            
         }
     }
+
 };
 
 int main(){
-    HashTable h;
-    h.insert("A" , "aaaaa");
-    h.insert("B" , "bbbbb");
-    h.insert("O","abcfll");
-    h.insert("C" , "ccccc");
-    h.traverse();
-    cout << "After updating value: " << endl;
-    h.insert("A" , "zzzzz");
-    h.traverse();
+    HashTable table;
+    table.add_record("AB" ,"FASTNU");
+    table.add_record("CD" , "CS");
+    table.add_record("ABC" , "UNI");
+    table.add_record("ABd" , "KHI");
+    table.add_record("b" , "aplha");
+    table.traverse();
+    cout << endl;
+    cout << "Searching for key " << endl;
+    table.search("ABd");
+    cout << endl;
+    table.remove("b");
+    cout << endl;
+    table.traverse();
+    return 0;
+
+
 }
